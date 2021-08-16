58aab8dd2bc86c0a22b1914643cdeb51bba4e2da
Francois Janson <jansonfrancois@gmail.com>
2021-08-16 20:27:31 +0200

    Using map more efficiently

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ const stepsMax = 50000;
 
 // créer un array d objet avec les props (clé + valeur)
 // Utiliser un map pour assigner les props
-const propsArray = [
+const boxes = [
   { icon: "local_drink", color: "#3A85FF", value: 1.5, unit: "L" },
   { icon: "directions_walk", color: "black", value: 3000, unit: "steps" },
   { icon: "favorite", color: "red", value: 120, unit: "bpm" },
@@ -19,41 +19,30 @@ const propsArray = [
 
 export class App extends React.Component {
   render() {
-    console.log("propsArray", propsArray.map((elem) => elem.icon)[0]);
+    // console.log("propsArray", boxes.map((elem) => elem.icon)[0]);
     return (
       <div className="container-fluid">
         <div className="row">
           {/* Water */}
-          <Box
-            icon={propsArray.map((elem) => elem.icon)[0]}
-            color={propsArray.map((elem) => elem.color)[0]}
-            value={propsArray.map((elem) => elem.value)[0]}
-            unit={propsArray.map((elem) => elem.unit)[0]}
-            primary
-          />
+          {/* <Box icon="local_drink" color="#3A85FF" value="1.5" unit="L" /> */}
           {/* Steps */}
           {/* <Box icon="directions_walk" color="black" value="3000" unit="steps" /> */}
-          <Box
-            icon={propsArray.map((elem) => elem.icon)[1]}
-            color={propsArray.map((elem) => elem.color)[1]}
-            value={propsArray.map((elem) => elem.value)[1]}
-            unit={propsArray.map((elem) => elem.unit)[1]}
-          />
           {/* Heart */}
-          <Box
-            icon={propsArray.map((elem) => elem.icon)[2]}
-            color={propsArray.map((elem) => elem.color)[2]}
-            value={propsArray.map((elem) => elem.value)[2]}
-            unit={propsArray.map((elem) => elem.unit)[2]}
-            primary
-          />
+          {/* <Box icon="favorite" color="red" value="120" unit="bpm" /> */}
           {/* Temperature */}
-          <Box
-            icon={propsArray.map((elem) => elem.icon)[3]}
-            color={propsArray.map((elem) => elem.color)[3]}
-            value={propsArray.map((elem) => elem.value)[3]}
-            unit={propsArray.map((elem) => elem.unit)[3]}
-          />
+          {/* <Box icon="wb_sunny" color="yellow" value="-10" unit="°C" /> */}
+
+          {/* ALL BOXES IN ONE OPERATION */}
+          {boxes.map((elem) => (
+            <Box
+              icon={elem.icon}
+              color={elem.color}
+              value={elem.value}
+              unit={elem.unit}
+              primary={boxes.indexOf(elem) % 2 ? true : false}
+            />
+          ))}
+
           <p>Heart : {heartMin}</p>
           <p>Temperature : {tempMin}</p>
           <p>Steps : {stepsMin}</p>
