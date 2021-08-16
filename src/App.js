a127099602d4826921c0bd6a503e5dbdce06dd6e
Francois Janson <jansonfrancois@gmail.com>
2021-08-16 15:17:25 +0200

    cleaning code

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Box from "./components/Box.js";
-import './styles/global.css';
+import "./styles/global.css";
 
 const tempMin = -20;
 const tempMax = 40;
@@ -9,20 +9,19 @@ const heartMax = 180;
 const stepsMin = 0;
 const stepsMax = 50000;
 
-
 export class App extends React.Component {
   render() {
     return (
       <div className="container-fluid">
         <div className="row">
           {/* Water */}
-          <Box icon="local_drink" color="#3A85FF" value="1.5" unit="L"/>
+          <Box icon="local_drink" color="#3A85FF" value="1.5" unit="L" />
           {/* Steps */}
-          <Box icon="directions_walk" color="black" value="3000" unit="steps"/>
+          <Box icon="directions_walk" color="black" value="3000" unit="steps" />
           {/* Heart */}
-          <Box icon="favorite" color="red" value="120" unit="bpm"/>
+          <Box icon="favorite" color="red" value="120" unit="bpm" />
           {/* Temperature */}
-          <Box icon="wb_sunny" color="yellow" value="-10" unit="°C"/>
+          <Box icon="wb_sunny" color="yellow" value="-10" unit="°C" />
           <p>Heart : {heartMin}</p>
           <p>Temperature : {tempMin}</p>
           <p>Steps : {stepsMin}</p>
