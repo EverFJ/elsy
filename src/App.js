cccf6484cc233a6db22c9742ca4f987e0426d6e2
Francois Janson <jansonfrancois@gmail.com>
2021-08-18 15:12:17 +0200

    adding sliders

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Box from "./components/Box";
+// import "./styles/global.css";
 
 const tempMin = -20;
 const tempMax = 40;
@@ -10,30 +11,89 @@ const stepsMax = 50000;
 
 // créer un array d objet avec les props (clé + valeur)
 // Utiliser un map pour assigner les props
-const boxes = [
-  { icon: "local_drink", color: "#3A85FF", value: 1.5, unit: "L" },
-  { icon: "directions_walk", color: "black", value: 3000, unit: "steps" },
-  { icon: "favorite", color: "red", value: 120, unit: "bpm" },
-  { icon: "wb_sunny", color: "yellow", value: -10, unit: "°C" },
-];
+
+// const boxes = [
+//   { icon: "local_drink", color: "#3A85FF", value: 1.5, unit: "L" },
+//   { icon: "directions_walk", color: "black", value: 3000, unit: "steps" },
+//   { icon: "favorite", color: "red", value: 120, unit: "bpm" },
+//   { icon: "wb_sunny", color: "yellow", value: -10, unit: "°C" },
+// ];
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      water: 0,
+      heart: 120,
+      temperature: -10,
+      steps: 3000,
+    };
+  }
+
+  onHeartChange = (e) => {
+    this.setState({
+      heart: e,
+    });
+  };
+  onTempChange = (e) => {
+    this.setState({
+      temperature: e,
+    });
+  };
+  onStepChange = (e) => {
+    this.setState({
+      steps: e,
+    });
+  };
+
   render() {
-    // console.log("propsArray", boxes.map((elem) => elem.icon)[0]);
     return (
       <div className="container-fluid">
         <div className="row">
           {/* Water */}
-          {/* <Box icon="local_drink" color="#3A85FF" value={1.5} unit="L" /> */}
+          <Box
+            icon="local_drink"
+            color="#3A85FF"
+            value={this.state.water}
+            unit="L"
+            primary
+            water
+          />
           {/* Steps */}
-          {/* <Box icon="directions_walk" color="black" value={3000} unit="steps" /> */}
+          <Box
+            icon="directions_walk"
+            color="black"
+            value={this.state.steps}
+            unit="steps"
+            min={stepsMin}
+            max={stepsMax}
+            onChange={this.onStepChange}
+          />
           {/* Heart */}
-          {/* <Box icon="favorite" color="red" value={120} unit="bpm" /> */}
+          <Box
+            icon="favorite"
+            color="red"
+            value={this.state.heart}
+            unit="bpm"
+            primary
+            min={heartMin}
+            max={heartMax}
+            onChange={this.onHeartChange}
+          />
           {/* Temperature */}
-          {/* <Box icon="wb_sunny" color="yellow" value={-10} unit="°C" /> */}
+          <Box
+            icon="wb_sunny"
+            color="yellow"
+            value={this.state.temperature}
+            unit="°C"
+            min={tempMin}
+            max={tempMax}
+            onChange={this.onTempChange}
+          />
 
           {/* ALL BOXES IN ONE OPERATION */}
-          {boxes.map((elem, index) => (
+          {/* {boxes.map((elem, index) => (
             <Box
               icon={elem.icon}
               color={elem.color}
@@ -41,7 +101,7 @@ class App extends React.Component {
               unit={elem.unit}
               primary={index % 2 === 0}
             />
-          ))}
+          ))} */}
 
           <p>Heart : {heartMin}</p>
           <p>Temperature : {tempMin}</p>
--- src/components/Box.jsx
+++ src/components/Box.jsx
@@ -10,20 +10,50 @@ const StyledBox = styled.div`
 
 class Box extends React.Component {
   render() {
-    // console.log(this.props);
-    return (
-      <StyledBox primary={this.props.primary} className="col-sm-3 col-6">
-        <span
-          style={{ color: this.props.color, fontSize: 100 }}
-          className="material-icons"
-        >
-          {this.props.icon}
-        </span>
-        <p>
-          {this.props.value} {this.props.unit}
-        </p>
-      </StyledBox>
-    );
+    if (this.props.water) {
+      return (
+        <StyledBox primary={this.props.primary} className="col-sm-3 col-6">
+          <span
+            style={{ color: this.props.color, fontSize: 100 }}
+            className="material-icons"
+          >
+            {this.props.icon}
+          </span>
+          <p>
+            {this.props.value} {this.props.unit}
+          </p>
+        </StyledBox>
+      );
+    } else {
+      return (
+        <StyledBox primary={this.props.primary} className="col-sm-3 col-6">
+          <span
+            style={{ color: this.props.color, fontSize: 100 }}
+            className="material-icons"
+          >
+            {this.props.icon}
+          </span>
+          <p>
+            {this.props.value} {this.props.unit}
+          </p>
+
+          <input
+            className="form-range"
+            type="range"
+            name="slider"
+            id="slider"
+            min={this.props.min}
+            max={this.props.max}
+            value={this.props.value}
+            onChange={() => {
+              // console.log("value", value);
+              console.log("this.props.value", this.props.value);
+              this.props.onChange();
+            }}
+          />
+        </StyledBox>
+      );
+    }
   }
 }
 export default Box;
